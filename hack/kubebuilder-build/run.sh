e1772922495176514d0a94e0ec1c038a76ad0073
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2022-10-02 22:43:00 +1100

    Checkpoint kubebuilder init

--- hack/kubebuilder-build/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t k8s-configmap-operator hack/kubebuilder-build/
-
-docker run -it --rm -v ${PWD}/:/workdir -u ${UID}:${UID} --workdir /workdir --entrypoint bash k8s-configmap-operator
